881211f58dddcc680bb52c3bfa1f7f147c48d033
Mohd Suhel Khan <sohailkhan00987002gmail.com>
2026-09-13 20:01:13 +0530

    remove express route from frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -13,10 +13,4 @@ function App() {
     </AuthProvider>
   )
 }
-app.get("/", (req, res) => {
-  res.json({
-    status: "success",
-    message: "Backend is running"
-  });
-});
 export default App
